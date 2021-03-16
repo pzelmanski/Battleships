a33155ec6ba19730c6d0cb4fabe63951859d7153
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-03-16 18:55:53 +0100

    replace shipCoordinates with new ones

--- Battleships/Battleships/Ship.cs
+++ Battleships/Battleships/Ship.cs
@@ -1,10 +1,28 @@
-﻿namespace Battleships
+﻿using System;
+using System.Collections.Generic;
+
+namespace Battleships
 {
+    public class ShipCoordinates
+    {
+        public Coordinates StartCoordinates { get; }
+        public Coordinates EndCoordinates { get; }
+        private ShipCoordinates(Coordinates startCoordinates, Coordinates endCoordinates)
+        {
+            StartCoordinates = startCoordinates;
+            EndCoordinates = endCoordinates;
+        }
+
+        public ShipCoordinates CreateCoordinates(int shipLength, List<ShipCoordinates> otherShipCoordinates)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    
     public class Ship
     {
-        public ShipCoordinates StartCoordinates { get; }
-        public ShipCoordinates EndCoordinates { get; }
-
+        private ShipCoordinates Coordinates { get; }
         public HitStatus GetHitStatus(Coordinates c)
         {
             return HitStatus.Miss;
