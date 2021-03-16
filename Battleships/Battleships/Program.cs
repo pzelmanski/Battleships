13ad2a360a207663769dde817d817f4c04d661ee
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-03-16 17:27:58 +0100

    empty project

---
+++ Battleships/Battleships/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Battleships
+{
+    /*
+        The program should create a 10x10 grid, 
+        and place several ships on the grid at random with the following sizes:
+            1x Battleship (5 squares)
+            2x Destroyers (4 squares)
+        The player enters or selects coordinates of the form “A5”,
+        where “A” is the column and “5” is the row, 
+        to specify a square to target. 
+        Shots result in hits, misses or sinks. 
+        The game ends when all ships are sunk.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Battleships/BattleshipsTests/BattleshipE2E.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace BattleshipsTests
+{
+    public class BattleshipE2E
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
