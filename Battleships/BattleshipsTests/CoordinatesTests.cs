10639da3e9045a1eaba6f707e075db0fe0f2f6e7
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-03-16 19:42:46 +0100

    moved all coordinates tests to single file

--- Battleships/BattleshipsTests/CoordinatesTests.cs
+++ Battleships/BattleshipsTests/CoordinatesTests.cs
@@ -1,4 +1,5 @@
-﻿using Battleships;
+﻿using System;
+using Battleships;
 using FluentAssertions;
 using Xunit;
 
@@ -39,6 +40,30 @@ namespace BattleshipsTests
         {
             Coordinates.TryCreateFromInput(input).Should().BeNull();
         }
+        
+        public class ShipRowCoordinatesTests
+        {
+            [Theory]
+            [InlineData(1, 1)]
+            [InlineData(1, 10)]
+            [InlineData(10, 1)]
+            [InlineData(5, 5)]
+            public void GivenCorrectRowAndColumn_ItShouldReturnCoordinates(int row, int column)
+            {
+                var result = Coordinates.CreateOrThrow(row, column);
+                result.RowIndex.Should().Be(row);
+                result.ColumnIndex.Should().Be(column);
+            }
+
+            [Theory]
+            [InlineData(0, 1)]
+            [InlineData(1, 0)]
+            [InlineData(11, 1)]
+            [InlineData(1, 11)]
+            public void GivenIncorrectRowOrColumn_ItShouldThrow(int row, int column)
+            {
+                Assert.Throws<InvalidOperationException>(() => Coordinates.CreateOrThrow(row, column));
+            }
 
         [Theory]
         [InlineData(0, 0, false)]
