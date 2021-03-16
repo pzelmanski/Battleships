2ff2112c3162283ff7654a8e119e0b440d1fbd1b
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-03-16 18:38:43 +0100

    added shipCoordinates

--- Battleships/Battleships/Board.cs
+++ Battleships/Battleships/Board.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Battleships
 {
+    public class ShipCoordinates
+    {
+        public readonly int Row;
+        public readonly int Column;
+
+        private ShipCoordinates(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public static ShipCoordinates CreateOrThrow(int row, int column)
+        {
+            if(row > 0 && row < 11 && column > 0 && column < 11)
+                return new ShipCoordinates(row, column);
+            throw new InvalidOperationException($"Trying to create invalid coordinates (row, col): ({row}, {column})");
+        }
+    }
+    
+    public class Ship
+    {
+        public ShipCoordinates StartCoordinates { get; }
+        public ShipCoordinates EndCoordinates { get; }
+    }
+    
     public class Board
     {
+        private List<Ship> _ships;
+        
         public void Print()
         {
             throw new NotImplementedException();
---
+++ Battleships/BattleshipsTests/ShipRowCoordinatesTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Battleships;
+using FluentAssertions;
+using Xunit;
+
+namespace BattleshipsTests
+{
+    public class ShipRowCoordinatesTests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 10)]
+        [InlineData(10, 1)]
+        [InlineData(5, 5)]
+        public void GivenCorrectRowAndColumn_ItShouldReturnCoordinates(int row, int column)
+        {
+            var result = ShipCoordinates.CreateOrThrow(row, column);
+            result.Row.Should().Be(row);
+            result.Column.Should().Be(column);
+        }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 0)]
+        [InlineData(11, 1)]
+        [InlineData(1, 11)]
+        public void GivenIncorrectRowOrColumn_ItShouldThrow(int row, int column)
+        {
+            Assert.Throws<InvalidOperationException>(() => ShipCoordinates.CreateOrThrow(row, column));
+        }
+    }
+}
