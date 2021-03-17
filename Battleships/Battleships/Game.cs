356f7182d9e912ee4a6654499d19a4c9709609bc
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-03-17 20:45:22 +0100

    added game generation

---
+++ Battleships/Battleships/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Battleships
+{
+    public enum HitStatus
+    {
+        Miss = 0,
+        Hit = 1,
+        Sink = 2
+    }
+
+    public class Game
+    {
+        public List<Ship> Ships { get; } 
+
+        public Game(IEnumerable<int> shipLengths)
+        {
+            var ships = new List<Ship>();
+            foreach (var l in shipLengths)
+            {
+                ships.Add(GenerateShip(l, ships));
+            }
+            Ships = ships;
+        }
+
+        private Ship GenerateShip(int length, List<Ship> currentShips)
+        {
+            var r = new Random();
+
+            Ship? newShip = null;
+            while (newShip is null)
+            {
+                var row = r.Next(1, 10);
+                var column = r.Next(1, 10);
+                var direction = (GridDirection) r.Next(1, 4);
+                newShip = ShipFactory.TryCreateShip(Coordinates.CreateOrThrow(row, column), direction, length, currentShips);
+            }
+
+            return newShip;
+        }
+    }
+}
---
+++ Battleships/BattleshipsTests/GameTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Battleships;
+using FluentAssertions;
+using Xunit;
+
+namespace BattleshipsTests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void GivenLengthsArray_ItShouldGenerateCorrectAmountOfShips()
+        {
+            var result = new Game(new[] {4, 4, 5});
+            result.Ships.Count.Should().Be(3);
+        }
+
+        // TODO: Its a flaky test
+        [Fact]
+        public void GivenLengthsArray_ShipsShouldNotCollide()
+        {
+            var result = new Game(new[] {4, 4, 5});
+            var r = result.Ships.SelectMany(x => x.Segments.Select(y => y.Coordinates)).ToList();
+            r.Distinct().Count().Should().Be(r.Count());
+        }
+    }
+}
