f5b602347941f04e922c26434a70e346d9fd9cbc
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-03-21 21:04:40 +0100

    added proper e2e test

---
+++ Battleships/BattleshipsTests/CoordinatesTestsPropertyBased.cs
@@ -0,0 +1,25 @@
+﻿using Battleships;
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
+
+namespace BattleshipsTests
+{
+    public static class UserInputGenerator
+    {
+        public static Arbitrary<int> Generate()
+        {
+            return Arb.Default.Int32().Filter(i => i >= 1 && i <= 10);
+        }
+    }
+    public class CoordinatesTests_PropertyBased
+    {
+        
+        [Property(Arbitrary = new[] { typeof(UserInputGenerator) })]
+        public Property NotEmpty(int i)
+        {
+            var c = Coordinates.CreateOrThrow(i, 1);
+            return (c.RowIndex == i && c.ColumnIndex == 1).ToProperty();
+        }
+    }
+}
--- Battleships/BattleshipsTests/GameTests.cs
+++ Battleships/BattleshipsTests/GameTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Battleships;
 using FluentAssertions;
@@ -18,16 +19,16 @@ namespace BattleshipsTests
         [Fact]
         public void GivenLengthsArray_WhenItsImpossibleToGenerateShips_ItShouldThrow()
         {
-            Assert.Throws<InvalidOperationException>(() => new Game(new [] {9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9}));
+            Assert.Throws<InvalidOperationException>(() => new Game(new[] {9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9}));
         }
-        
+
         [Fact]
         public void GivenLengthsArray_ItShouldGenerateShipsWithUniqueIds()
         {
             var result = new Game(new[] {4, 4, 5});
             result.Ships.Select(x => x.ShipId).Distinct().Should().HaveCount(3, result.ToString());
         }
-        
+
         [Fact]
         public void GivenAnotherLengthsArray_ItShouldGenerateCorrectAmountOfShips()
         {
@@ -48,18 +49,58 @@ namespace BattleshipsTests
         }
 
         [Fact]
-        public void GivenGameWithSingleShip_WhenHitAndSink_ItShouldReturnCorrectHitStatus()
+        public void GivenGameWithSingleShip_WhenMissHitAndSink_ItShouldReturnCorrectHitStatus()
         {
             // Arrange
             var game = new Game(new[] {2});
-            var coordinates = game.Ships.Single().Segments.Select(x => x.Coordinates).ToList();
+            var hitCoordinates = game.Ships.Single().Segments.Select(x => x.Coordinates).ToList();
+
+            var missCoordinates = Coordinates.CreateOrThrow(1, 1);
+            if (hitCoordinates.Contains(missCoordinates))
+                missCoordinates = Coordinates.CreateOrThrow(5, 5);
 
             // Act && Assert
-            game.NextRound(coordinates[0]).Should().Be(HitStatus.Hit, game.ToString());
+            game.NextRound(missCoordinates).Should().Be(HitStatus.Miss, game.ToString());
             game.IsFinished().Should().BeFalse(game.ToString());
 
-            game.NextRound(coordinates[1]).Should().Be(HitStatus.Sink, game.ToString());
+            game.NextRound(hitCoordinates[0]).Should().Be(HitStatus.Hit, game.ToString());
+            game.IsFinished().Should().BeFalse(game.ToString());
+
+            game.NextRound(hitCoordinates[1]).Should().Be(HitStatus.Sink, game.ToString());
             game.IsFinished().Should().BeTrue(game.ToString());
+        }
+
+        [Fact]
+        public void GivenGameWithThreeShips_WhenPlayingFromBeginningToEnd_ItShouldActProperly()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                // Arrange
+                var game = new Game(new[] {4, 4, 5});
+
+                var ship1Coordinates = game.Ships[0].Segments.Select(x => x.Coordinates).ToList();
+                var ship2Coordinates = game.Ships[1].Segments.Select(x => x.Coordinates).ToList();
+                var ship3Coordinates = game.Ships[2].Segments.Select(x => x.Coordinates).ToList();
+
+                // Act & Assert
+                HitAndAssertHitStatus(game, ship1Coordinates);
+                HitAndAssertHitStatus(game, ship2Coordinates);
+                HitAndAssertHitStatus(game, ship3Coordinates);
+                game.IsFinished().Should().BeTrue();
+            }
+
+            static void HitAndAssertHitStatus(Game g, List<Coordinates> shipCoordinates)
+            {
+                for (var i = 0; i < shipCoordinates.Count; i++)
+                {
+                    var hitStatus = g.NextRound(shipCoordinates[i]);
+                    var expectedHitStatus = i == shipCoordinates.Count - 1
+                        ? HitStatus.Sink
+                        : HitStatus.Hit;
+                    hitStatus.Should().Be(expectedHitStatus,
+                        $"Coordinates of hit: ({shipCoordinates[i].ColumnIndex}, {shipCoordinates[i].RowIndex})\n" + g);
+                }
+            }
         }
 
         [Fact]
