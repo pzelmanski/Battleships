8561b50b4dd7033df39250b3d1e9482512ed59df
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-05-06 20:55:44 +0200

    simplified and extended initialization within board test

--- Battleships/FattleshipsTests/InitTests.fs
+++ Battleships/FattleshipsTests/InitTests.fs
@@ -4,24 +4,20 @@ open Domain
 open Xunit
 open FsUnit
 
-let isWithinBoard coord =
+let isCoordWithinBoard coord =
     coord >= 1 && coord <= 10
+
+let isShipWithinBoard ship =
+    ship.Coordinates
+    |> List.map (fun x -> x.Coordinate)
+    |> List.map (fun x -> (isCoordWithinBoard x.Col && isCoordWithinBoard x.Row))
+    |> List.fold (fun x y -> x && y) true
 
 [<Fact>]
 let ``should init single ship correctly``() =
     let ships = Init.InitGame [ShipLength 1;ShipLength 2]
     Assert.Equal(2, List.length ships)
-    ships.[0].Coordinates
-    |> List.map (fun x -> x.Coordinate)
-    |> List.map (fun x -> (isWithinBoard x.Col, isWithinBoard x.Row))
-    |> List.map (fun (x, y) -> x && y)
-    |> List.fold (fun x y -> x && y) true
-    |> should equal true
-    
-    ships.[1].Coordinates
-    |> List.map (fun x -> x.Coordinate)
-    |> List.map (fun x -> (isWithinBoard x.Col, isWithinBoard x.Row))
-    |> List.map (fun (x, y) -> x && y)
+    List.map isShipWithinBoard ships
     |> List.fold (fun x y -> x && y) true
     |> should equal true
     
