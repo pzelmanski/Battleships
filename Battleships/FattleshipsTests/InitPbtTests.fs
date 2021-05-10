bbc6cdfe83de2aae77336c2c205f787d10b66944
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-05-10 22:51:13 +0200

    first try at pbt

---
+++ Battleships/FattleshipsTests/InitPbtTests.fs
@@ -0,0 +1,34 @@
+﻿module InitPbtTests
+
+open FsCheck
+open FsCheck.Xunit
+
+open Domain
+type TestShipLength = 
+    static member genSingleShipLength() = 
+      gen { let! i = Gen.choose (1, 6) 
+            return ShipLength i }
+      |> Arb.fromGen
+
+let isCoordWithinBoard coord =
+    coord >= 1 && coord <= 10
+
+let isShipWithinBoard ship =
+    ship.Coordinates
+    |> List.map (fun x -> x.Coordinate)
+    |> List.map (fun x -> (isCoordWithinBoard x.Col && isCoordWithinBoard x.Row))
+    |> List.fold (fun x y -> x && y) true
+let allShipsWithinBoard ships =
+    List.map isShipWithinBoard ships
+    |> List.fold (fun x y -> x && y) true 
+    
+
+let t1 (len: ShipLength) =
+    Init.InitGame [len]
+    |> allShipsWithinBoard
+
+[<Properties( Arbitrary=[| typeof<TestShipLength> |] )>]
+module CheckInit = 
+    [<Property>]
+    let ``test test`` (len: ShipLength) =
+        t1 len
---
+++ Battleships/FattleshipsTests/pbtTest.fs
@@ -0,0 +1,41 @@
+﻿module pbtTest
+// TODO: Delete this later
+open FsCheck
+open FsCheck.Xunit
+
+type Positive =
+    static member Double() =
+        Arb.Default.Float()
+        |> Arb.mapFilter abs (fun t -> t > 0.0)
+
+type Negative =
+    static member Double() =
+        Arb.Default.Float()
+        |> Arb.mapFilter (abs >> ((-) 0.0)) (fun t -> t < 0.0)
+
+type Zero =
+    static member Double() =
+        0.0
+        |> Gen.constant
+        |> Arb.fromGen
+
+[<assembly: Properties( Arbitrary = [| typeof<Zero> |])>] do()
+module ModuleWithoutProperties =
+    [<Property>]
+    let ``should use Arb instances from assembly``(underTest:float) =
+        underTest = 0.0
+
+    [<Property( Arbitrary=[| typeof<Positive> |] )>]
+    let ``should use Arb instance on method``(underTest:float) =
+        underTest > 0.0
+
+[<Properties( Arbitrary=[| typeof<Negative> |] )>]
+module ModuleWithProperties =
+
+    [<Property>]
+    let ``should use Arb instances from enclosing module``(underTest:float) =
+        underTest < 0.0
+
+    [<Property( Arbitrary=[| typeof<Positive> |] )>]
+    let ``should use Arb instance on method``(underTest:float) =
+        underTest > 0.0
