6d8db29d889332e7497351ed5cbb29c0be05c408
Pawel Zelmanski <pzelmanski1@gmail.com>
2021-05-13 22:38:10 +0200

    added hit tests

---
+++ Battleships/FattleshipsTests/ShootTests.fs
@@ -0,0 +1,48 @@
+﻿module ShootTests
+
+open Xunit
+open Domain
+open GameLoop
+open FsUnit
+
+
+[<Fact>]
+let ``shoot 1`` () =
+    let ship =
+        { Coordinates =
+              [ { Coordinate = { Row = 1; Col = 1 }
+                  IsHit = false } ]
+          Id = 1 }
+
+    let hit = { Row = 1; Col = 1 }
+
+    shoot [ ship ] hit
+    |> fun x -> x.RoundResult
+    |> should equal RoundResult.Sink
+
+[<Fact>]
+let ``shoot 2`` () =
+    let ship =
+        { Coordinates =
+              [ { Coordinate = { Row = 1; Col = 1 }
+                  IsHit = false }
+                { Coordinate = { Row = 2; Col = 1 }
+                  IsHit = false } ]
+          Id = 1 }
+
+    let miss = {Row = 2; Col = 2}
+    let hit = { Row = 1; Col = 1 }
+    let sink = {Row = 2; Col = 1}
+    
+    shoot [ship] miss
+    |> fun x -> x.RoundResult
+    |> should equal RoundResult.Miss
+    
+    let hitResult = shoot [ ship ] hit
+    hitResult
+    |> fun x -> x.RoundResult
+    |> should equal RoundResult.Hit
+    
+    shoot hitResult.Ships sink
+    |> fun x -> x.RoundResult
+    |> should equal RoundResult.Sink
